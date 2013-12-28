dc0bc61372fbe6feab9f6afcefc20582f8fd1a6d
Daniel Carter <geddoe@gmail.com>
2013-12-27 19:42:00 -0500

    Association tests for card.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -4,4 +4,7 @@ describe Card do
   it { should validate_presence_of :front }
   it { should validate_presence_of :back }
   it { should validate_presence_of :user_id }
+
+  it { should have_many(:tags).through(:taggings) }
+  it { should have_many(:taggings).dependent(:destroy) }
 end
