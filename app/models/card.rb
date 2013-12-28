6a4489236a39ef3e08ede15e0392449c5b99373a
Daniel Carter <geddoe@gmail.com>
2013-12-27 19:47:20 -0500

    Associations for tags and cards.

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,9 @@ class Card < ActiveRecord::Base
   has_many :taggings,
     inverse_of: :card,
     dependent: :destroy
+  has_many :tags,
+    through: :taggings,
+    inverse_of: :cards
 
   validates_presence_of :front
   validates_presence_of :back
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,9 @@ class Tag < ActiveRecord::Base
   has_many :taggings,
     inverse_of: :tag,
     dependent: :destroy
+  has_many :cards,
+    through: :taggings,
+    inverse_of: :tags
 
   validates_presence_of :name
   validates_presence_of :user_id
