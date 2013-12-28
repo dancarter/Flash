b9965594166279824ec5c2f4a2501f6cccf60f38
Daniel Carter <geddoe@gmail.com>
2013-12-27 19:43:50 -0500

    Association tests for taggings.

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -3,4 +3,7 @@ require 'spec_helper'
 describe Tagging do
   it { should validate_presence_of :card_id }
   it { should validate_presence_of :tag_id }
+
+  it { should belong_to(:card) }
+  it { should belong_to(:tag) }
 end
