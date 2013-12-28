966140127d1a9834fa92d59cc8d946b8576dced0
Daniel Carter <geddoe@gmail.com>
2013-12-27 20:08:38 -0500

    User validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe User do
   it { should validate_presence_of :email }
   it { should validate_presence_of :username }
-  it { should validate_presence_of :encrypted_password }
 
   it { should_not have_valid(:email).when("hithere","hi@there","hithere.com") }
   it { should have_valid(:email).when("hi@there.com","yolo@gmail.com") }
