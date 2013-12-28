b95d66c610b7c93387edae0b554fdae68eaf7a3f
Daniel Carter <geddoe@gmail.com>
2013-12-27 19:46:06 -0500

    Associations for tags and taggings.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
+  has_many :taggings,
+    inverse_of: :tag,
+    dependent: :destroy
+
   validates_presence_of :name
   validates_presence_of :user_id
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,8 @@
 class Tagging < ActiveRecord::Base
   belongs_to :card,
     inverse_of: :taggings
+  belongs_to :tag,
+    inverse_of: :taggings
 
   validates_presence_of :tag_id
   validates_presence_of :card_id
