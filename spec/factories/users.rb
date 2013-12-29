92bf9ea03b7efdcd99962945c02a4bbbcc1a23e2
Daniel Carter <geddoe@gmail.com>
2013-12-29 01:36:23 -0500

    Fixed user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
   factory :user do
     username 'TheUser'
     email 'fake@real.com'
-    encrypted_password '$2a$10$3shmtWIkLjXPhBWwZ6qlVelCO1NmI9KHsUmX35zrrlDVhjV4GEppC'
+    password 'passw0rd'
   end
 end
