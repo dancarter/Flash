b244df01689adaa5825328ec66f9dbcf4dc43701
Daniel Carter <geddoe@gmail.com>
2014-01-09 13:36:18 -0500

    Missed spec with acceptance tests in last commit.

---
+++ spec/features/share/user_views_public_tags_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+feature "User views public tags", %q{
+  As a registered user
+  I want to search public tags
+  So that I can see other users' cards
+} do
+
+  #   Acceptance Criteria:
+  # * I can see a list of public tags
+  # * I can search to see tags with specific names
+  # * If there are tags with names matching my search they are shown
+  # * Otherwise, a no tags found message is shown
+  # * I can click on a tag to see the cards it contains
+
+  before :each do
+    user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
+    user1.confirm!
+    user2.confirm!
+    @tag1 = FactoryGirl.create(:tag, user: user1, share: true)
+    @tag2 = FactoryGirl.create(:tag, user: user1, share: true)
+    sign_in_as(user2)
+  end
+
+  context "user views public tags" do
+
+    it "should list public tags" do
+      visit share_path
+
+      expect(page).to have_content(@tag1.name)
+      expect(page).to have_content(@tag2.name)
+    end
+
+    it "should not list private tags" do
+      tag3 = FactoryGirl.create(:tag, user: user1)
+
+      visit share_path
+
+      expect(page).to_not have_content(tag3.name)
+    end
+
+  end
+
+  context "user searches for public tag" do
+
+    it "should show tag with matching name" do
+      visit share_path
+      fill_in "Search", with: @tag1.name
+
+      expect(page).to have_content(@tag1.name)
+    end
+
+    it "should not show tag without matching name" do
+      visit share_path
+      fill_in "Search", with: @tag1.name
+
+      expect(page).to_not have_content(@tag2.name)
+    end
+
+  end
+
+  context "user clicks public tag to view cards" do
+
+    it "should show the cards under chosen tag" do
+      card = FactoryGirl.create(:card, tag: @tag1, user: user1)
+      visit share_path
+      click_on @tag1.name
+
+      expect(page).to have_content(card.front)
+      expect(page).to have_content(card.back)
+    end
+
+  end
+
+end
