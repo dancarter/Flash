3f9ee56b3ed1d1b56082c0e47c0e1bb9b0040819
Daniel Carter <geddoe@gmail.com>
2014-01-13 18:24:43 -0500

    Production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,12 @@ Flash::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'flash.herokuapp.com'
+  }
 end
