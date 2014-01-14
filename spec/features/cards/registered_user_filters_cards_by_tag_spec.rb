500f93384fb1784e77e4e3a51ab34b945268661d
Daniel Carter <geddoe@gmail.com>
2014-01-14 15:58:38 -0500

    Acceptance

---
+++ spec/features/cards/registered_user_filters_cards_by_tag_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+feature "User filters cards by tag", %q{
+    As a registered user
+    I want to filter my card list by tag
+    So that I can find cards for specific tags quickly
+} do
+
+  # Acceptance Criteria
+  # * I can choose a tag when viewing my card list
+  # * Cards not under that tag are filtered out
+  # * If no tag is chosen, all cards are listed
+
+  let(:user) { FactoryGirl.create(:user) }
+  let!(:tag) { FactoryGirl.create(:tag, user: user) }
+  let!(:card1) { FactoryGirl.create(:card, user: user) }
+  let!(:card2) { FactoryGirl.create(:card, user: user) }
+  let!(:tagging) { FactoryGirl.create(:tagging, card: card1, tag: tag) }
+
+  before :each do
+    sign_in_as(user)
+  end
+
+  context "User selects no tag" do
+
+    it "displays all cards" do
+      visit cards_path
+
+      expect(page).to have_content(card1.front)
+      expect(page).to have_content(card2.front)
+    end
+
+  end
+
+  context "User selects a tag" do
+    card2 = FactoryGirl.create(:card, user: user)
+
+    it "should only show cards for that tag" do
+      visit cards_path
+
+      select tag1.name, from: Tags
+
+      expect(page).to have_content(card1.front)
+      expect(page).to_not have_content(card2.front)
+    end
+
+  end
+
+end
